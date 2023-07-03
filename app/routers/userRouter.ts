c1bb5f3763c93a418c0fd47ec3f75caa9e7b60dc
soumik9 <soumik.ahammed.9@gmail.com>
2023-07-03 14:29:22 +0600

    update profile

--- app/routers/userRouter.ts
+++ app/routers/userRouter.ts
@@ -8,15 +8,16 @@ import DeleteUser from '../controllers/user/DeleteUser';
 import auth from '../middlewares/auth';
 import { CON_ADMIN_ROLE, CON_BUYER_ROLE, CON_SELLER_ROLE } from '../../utils/constatnts';
 import MyProfile from '../controllers/user/MyProfile';
+import UpdateProfile from '../controllers/user/UpdateProfile';
 
 //routes
 router.get('/my-profile', auth(CON_BUYER_ROLE, CON_SELLER_ROLE), MyProfile);
-
 router.get('/:id', auth(CON_ADMIN_ROLE), GetUser);
-
 router.get('/', auth(CON_ADMIN_ROLE), GetUsers);
 
+router.patch('/my-profile', auth(CON_BUYER_ROLE, CON_SELLER_ROLE), UpdateProfile);
 router.patch('/:id', auth(CON_ADMIN_ROLE), UpdateUser);
+
 router.delete('/:id', auth(CON_ADMIN_ROLE), DeleteUser);
 
 export default router;
